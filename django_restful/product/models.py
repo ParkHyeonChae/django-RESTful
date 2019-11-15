84f65b0814328d2578bc70845bf1c05ae19d8faa
ParkHyeonChae <park19996@naver.com>
2019-11-15 13:35:26 +0900

    Model 구성, migration

--- django_restful/product/models.py
+++ django_restful/product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# 제품 클래스
+class Product(models.Model):
+    name = models.CharField(max_length=256, verbose_name='상품명')
+    price = models.IntegerField(verbose_name='상품가격')
+    description = models.TextField(verbose_name='상품설명')
+    stuck = models.IntegerField(verbose_name='제고')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
+
+    class Meta:
+        db_table = 'django_product'
+        verbose_name = '상품'
+        verbose_name_plural = '상품'
--- django_restful/user/models.py
+++ django_restful/user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# 회원 클래스
+class User(models.Model):
+    email = models.EmailField(verbose_name='이메일')
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
+
+    class Meta:
+        db_table = 'django_user'
+        verbose_name = '사용자'
+        verbose_name_plural = '사용자'
+
+
+
+# 1. settings에 app 등록
+# 2. python manage.py makemigrations
+# 3. python manage.py migrate
