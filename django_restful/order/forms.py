20611ea5bf2f802d5ed3158799ff870c28e0abb9
ParkHyeonChae <park19996@naver.com>
2019-11-22 03:34:23 +0900

    user session 접근, user order구현

--- django_restful/order/forms.py
+++ django_restful/order/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Order
+from product.models import Product
+from user.models import User
 
 class RegisterForm(forms.Form):
+    def __init__(self, request, *args, **kwargs): # request를 전달할 수 있게 생성자 함수 생성
+        super().__init__(*args, **kwargs)
+        self.request = request
+
     quantity = forms.IntegerField(
         error_messages={
             'required': '수량을 입력해주세요'
@@ -18,3 +24,18 @@ class RegisterForm(forms.Form):
         cleaned_data = super().clean()
         quantity = cleaned_data.get('quantity')
         product = cleaned_data.get('product')
+        user = self.request.session.get('user')
+
+        if quantity and product and user:
+            order = Order(
+                quantity=quantity,
+                product=Product.objects.get(pk=product),
+                user=User.objects.get(email=user)
+            )
+            order.save()
+        else:
+            self.product = product
+            self.add_error('quantity', '값이 없습니다.')
+            self.add_error('product', '값이 없습니다.')
+
+        #print(self.request.session) # 사용자 세션 접근
--- django_restful/order/views.py
+++ django_restful/order/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from .forms import RegisterForm
 
 class OrderCreate(FormView):
     form_class = RegisterForm
-    success_url = '/product'
+    success_url = '/product/'
+
+    def form_invalid(self, form):
+        return redirect('/product/' + str(form.product))
+
+    def get_form_kwargs(self, **kwargs): # form을 생성할때 어떤 인자값을 전달할지 결정
+        kw = super().get_form_kwargs(**kwargs) #  기존 자동으로 생성되는 인자값에 request도 포함시켜 생성
+        kw.update({
+            'request': self.request
+        })
+        return kw
