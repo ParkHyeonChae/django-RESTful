ece241a17991271f6e587e12d32520d7c3949c68
ParkHyeonChae <park19996@naver.com>
2019-11-26 18:58:22 +0900

    order form, view 분리

--- django_restful/order/views.py
+++ django_restful/order/views.py
@@ -5,14 +5,31 @@ from django.utils.decorators import method_decorator
 from user.decorators import login_required
 from .forms import RegisterForm
 from .models import Order
+from django.db import transaction # transaction : 일련의 동작을 하나의 동작으로 처리, 전체가 다 성공하면 성공, 하나라도 실패하면 롤백 (주문시 제고도 감소)
+from product.models import Product
+from user.models import User
 
 @method_decorator(login_required, name='dispatch')
 class OrderCreate(FormView):
     form_class = RegisterForm
     success_url = '/product/'
     
+    def form_valid(self, form):
+        with transaction.atomic():
+            prod = Product.objects.get(pk=form.data.get('product'))
+            order = Order(
+                quantity=form.data.get('quantity'),
+                product=prod,
+                user=User.objects.get(email=self.request.session.get('user'))
+            )
+            order.save()
+            prod.stock -= int(form.data.get('quantity'))
+            prod.save()
+        
+        return super().form_valid(form)
+
     def form_invalid(self, form):
-        return redirect('/product/' + str(form.product))
+        return redirect('/product/' + str(form.data.get('product')))
 
     def get_form_kwargs(self, **kwargs): # form을 생성할때 어떤 인자값을 전달할지 결정
         kw = super().get_form_kwargs(**kwargs) #  기존 자동으로 생성되는 인자값에 request도 포함시켜 생성
